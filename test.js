cc39bf6fe8bd2aa805fb443d7d32e302febab592
hanbinchoi <wkghskak@naver.com>
2023-04-12 13:21:58 +0900

    230412

--- test.js
+++ test.js
@@ -1,5 +1,25 @@
 function solution(n) {
   var answer = [];
-  return answer;
+  for (let i = 2; i <= n; i++) {
+    if (n % i == 0) {
+      answer.push(i);
+    }
+  }
+  console.log(answer);
+  let k = [];
+  answer.forEach((element) => {
+    let i = 2;
+    while (i != element + 1) {
+      if (element % i == 0) {
+        if (element === i) {
+          k.push(i);
+        } else {
+          break;
+        }
+      }
+      i++;
+    }
+  });
+  return k;
 }
-console.log(solution(12));
+console.log(solution(8));
