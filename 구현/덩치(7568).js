37c3be80c36b685b3f7cf102f9a5f249ac7207f0
hanbinchoi <wkghskak@naver.com>
2023-07-28 15:55:03 +0900

    등수 구하기

--- 구현/덩치(7568).js
+++ 구현/덩치(7568).js
@@ -12,7 +12,8 @@
 // C	(88, 186)	1
 // D	(60, 175)	2
 // E	(46, 155)	5
-// 위 표에서 C보다 더 큰 덩치의 사람이 없으므로 C는 1등이 된다. 그리고 A, B, D 각각의 덩치보다 큰 사람은 C뿐이므로 이들은 모두 2등이 된다. 그리고 E보다 큰 덩치는 A, B, C, D 이렇게 4명이므로 E의 덩치는 5등이 된다. 위 경우에 3등과 4등은 존재하지 않는다. 여러분은 학생 N명의 몸무게와 키가 담긴 입력을 읽어서 각 사람의 덩치 등수를 계산하여 출력해야 한다.
+// 위 표에서 C보다 더 큰 덩치의 사람이 없
+//으므로 C는 1등이 된다. 그리고 A, B, D 각각의 덩치보다 큰 사람은 C뿐이므로 이들은 모두 2등이 된다. 그리고 E보다 큰 덩치는 A, B, C, D 이렇게 4명이므로 E의 덩치는 5등이 된다. 위 경우에 3등과 4등은 존재하지 않는다. 여러분은 학생 N명의 몸무게와 키가 담긴 입력을 읽어서 각 사람의 덩치 등수를 계산하여 출력해야 한다.
 
 // [문제 링크]
 // : https://www.acmicpc.net/problem/7568
@@ -21,50 +22,94 @@
 let fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./ex.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
-const n = +input[0];
-const arr = [];
-for (let i = 1; i <= n; i++) {
-  arr.push(input[i].split(" ").map(Number));
-}
-const newArr = [];
-arr.forEach((ele) => {
-  // 무게와 키가 둘다 큰 사람이 있는지 확인.
-  const bigger = arr.filter((e) => e[0] > ele[0] && e[1] > ele[1]).length;
-  // 등수를 저장해야 하므로 +1
-  newArr.push(bigger + 1);
-});
-console.log(newArr.join(" "));
+const n = +input.shift();
 
-// 뻘짓한 코드
-// // 무게와 키가 큰 사람이 있는 명수로 sort
-// newArr.sort((a, b) => a[1] - b[1]);
-// let num = 1;
-// let k = 1;
-// const rank = [];
-// // 등수배열을 만든다.
-// for (let i = 0; i < newArr.length; i++) {
-//   // 배열이 비어잇으면 첫번째 사람은 1등
-//   if (i === 0) rank.push([...newArr[i], num]);
-//   else {
-//     // 배열이 있다면 이전에 사람과 비교
-//     const prev = rank[rank.length - 1][1];
-//     // 명수가 같으면 같은 등수
-//     if (prev === newArr[i][1]) {
-//       rank.push([...newArr[i], num]);
-//       // 다음 등수는 앞에 사람만큼 + 해줘야 하므로 k를 증가
-//       k++;
-//     } else {
-//       // 등수를 앞에 사람만큼 더해준다
-//       num += k;
-//       rank.push([...newArr[i], num]);
-//       // 다시 앞에 사람은 1로 초기화
-//       k = 1;
-//     }
-//   }
-// }
-// console.log(
-//   rank
-//     .sort((a, b) => a[0] - b[0])
-//     .map((ele) => ele[2])
-//     .join(" ")
-// );
+// 덩치 순 정렬된 queue
+const queue = [];
+// 순위를 담을 배열
+let rank = [];
+let rest = [];
+let rankRest = [];
+let count = 0;
+for (let i = 0; i < n; i++) {
+  const [w, h] = input[i].split(" ").map(Number);
+
+  // 초기 큐가 비워있는 상태에는 첫번째 값 그대로 푸쉬, 랭크도 1로 셋팅
+  if (i === 0) {
+    rank.push(1);
+    queue.push([w, h]);
+    count = 1;
+  } else
+    while (true) {
+      // 제일 커서 가장 맨 앞에 추가되는 경우
+      if (queue.length === 0) {
+        // 푸쉬하고 나머지 원소들 푸쉬
+        queue.push([w, h]);
+        queue.push(...rest);
+        // 랭크는 1푸쉬하고 나머지 랭크들+1 해서 푸쉬
+        rank.push(1);
+        rank.push(...rankRest.map((ele) => ele + 1));
+        rest = [];
+        rankRest = [];
+        break;
+      }
+      // 앞사람보다 키, 몸무게 둘다 작으면 그냥 뒤에 배치
+      if (queue[queue.length - 1][0] > w && queue[queue.length - 1][1] > h) {
+        // 현재 원소를 맨 뒤에 추가
+        queue.push([w, h]);
+        // 나머지 배열들도 그 뒤에 추가
+        queue.push(...rest);
+        // 랭크는 현재 쌓인 count만큼 더해줌
+        rank.push(rank[rank.length - 1] + count);
+        // 나머지 뒤에 오는 원소도 count 만큼 더해줌
+        rank.push(...rankRest.map((ele) => ele + count));
+        // 다시 count 1로 셋팅
+        count = 1;
+        rest = [];
+        rankRest = [];
+        break;
+        // 앞 사람보다 키 혹은 몸무게만 커서 둘이 같은 등수일 경우
+      } else if (
+        queue[queue.length - 1][0] > w &&
+        queue[queue.length - 1][1] < h
+      ) {
+        queue.push([w, h]);
+        queue.push(...rest);
+        // 랭크는 앞에 있는 원소와 동일
+        rank.push(rank[rank.length - 1]);
+        // 나머지는 쌓인만큼 더해줌
+        rank.push(...rankRest.map((ele) => ele + count));
+        rest = [];
+        rankRest = [];
+        // count는 1만큼 쌓임
+        count++;
+        break;
+      } else if (
+        queue[queue.length - 1][0] < w &&
+        queue[queue.length - 1][1] > h
+      ) {
+        queue.push([w, h]);
+        queue.push(...rest);
+        rank.push(rank[rank.length - 1]);
+        rank.push(...rankRest.map((ele) => ele + count));
+        rest = [];
+        rankRest = [];
+        count++;
+        break;
+      }
+      // 앞사람보다 더 커서 앞으로 들어가야 하는 경우
+      else {
+        // 맨 뒤에 원소를 빼서 나머지 배열에 추가
+        rest.push(queue.pop());
+        // 랭크도 마찬가지
+        rankRest.push(rank.pop());
+      }
+    }
+}
+let answer = "";
+for (let i = 0; i < n; i++) {
+  const [w, h] = input[i].split(" ").map(Number);
+  const idx = queue.findIndex((ele) => ele[0] === w && ele[1] === h);
+  answer += rank[idx] + " ";
+}
+console.log(answer);
---
+++ 구현/등수 구하기(1205).js
@@ -0,0 +1,38 @@
+// [문제 이름]
+// : 등수 구하기
+
+// [문제 설명]
+// : 태수가 즐겨하는 디제이맥스 게임은 각각의 노래마다 랭킹 리스트가 있다. 이것은 매번 게임할 때 마다 얻는 점수가 비오름차순으로 저장되어 있는 것이다.
+
+// 이 랭킹 리스트의 등수는 보통 위에서부터 몇 번째 있는 점수인지로 결정한다. 하지만, 같은 점수가 있을 때는 그러한 점수의 등수 중에 가장 작은 등수가 된다.
+
+// 예를 들어 랭킹 리스트가 100, 90, 90, 80일 때 각각의 등수는 1, 2, 2, 4등이 된다
+
+// 랭킹 리스트에 올라 갈 수 있는 점수의 개수 P가 주어진다. 그리고 리스트에 있는 점수 N개가 비오름차순으로 주어지고, 태수의 새로운 점수가 주어진다. 이때, 태수의 새로운 점수가 랭킹 리스트에서 몇 등 하는지 구하는 프로그램을 작성하시오. 만약 점수가 랭킹 리스트에 올라갈 수 없을 정도로 낮다면 -1을 출력한다.
+
+// 만약, 랭킹 리스트가 꽉 차있을 때, 새 점수가 이전 점수보다 더 좋을 때만 점수가 바뀐다.
+
+// [문제 링크]
+// : https://www.acmicpc.net/problem/1205
+
+// 백준 문제풀이용 코드
+let fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./ex.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+// 개수, 새로운 점수, 랭킹에 올라갈수 있는 개수
+const [n, newP, p] = input.shift().split(" ").map(Number);
+if (input.length === 0) {
+  console.log(1);
+  return;
+}
+// 랭킹
+const rank = input.shift().split(" ").map(Number);
+const highRank = rank.filter((ele) => ele >= newP);
+
+if (highRank.length >= p) console.log(-1);
+else if (highRank[highRank.length - 1] === newP)
+  console.log(highRank.filter((ele) => ele > newP).length + 1);
+else {
+  console.log(highRank.length + 1);
+}
