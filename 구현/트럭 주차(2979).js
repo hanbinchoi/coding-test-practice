82793a37f91c52a469b74f2ed74518d8c5b5b306
hanbinchoi <wkghskak@naver.com>
2024-01-25 17:35:15 +0900

    [구현]트럭 주차

--- 구현/트럭 주차(2979).js
+++ 구현/트럭 주차(2979).js
@@ -20,43 +20,69 @@ let input = fs.readFileSync(filePath).toString().split("\n");
 
 let times = [];
 
+let prices = input[0].split(" ").map(Number);
+
+let ins = [];
+let outs = [];
+
 for (let i = 1; i <= 3; i++) {
-  times.push(input[i].split(" ").map(Number));
+  const [a, b] = input[i].split(" ").map(Number);
+  ins.push(a);
+  outs.push(b);
 }
-
-// 각 주차 대수마다 가격을 저장한 변수
-const [a, b, c] = input[0].split(" ").map(Number);
-
-// 나간시간과 들어온 시간을 각각 배열에 담는다.
-let inTime = [];
-let outTime = [];
-times.forEach((ele) => {
-  inTime.push(ele[0]);
-  outTime.push(ele[1]);
-});
-
-// 정답 변수
-let answer = 0;
-// 현재 주차중인 트럭 대수
 let num = 0;
+let answer = 0;
 
-// 주어지는 시간인 1부터 100까지 순회
 for (let i = 1; i <= 100; i++) {
-  // 현재 시간을 i라고 놓고 i시간에 들어오는 차와 나가는 차의 대수를 찾음.
-  const inNum = inTime.filter((ele) => ele === i).length;
-  const outNum = outTime.filter((ele) => ele === i).length;
+  const inNum = ins.filter((ele) => ele == i).length;
+  const outNum = outs.filter((ele) => ele == i).length;
 
-  // 1대라도 있으면 그 대수만큼 현재 주차 대수의 더하거나 뺀다.
-  if (inNum) {
-    num += inNum;
-  }
-  if (outNum) {
-    num -= outNum;
-  }
+  if (inNum) num += inNum;
+  if (outNum) num -= outNum;
 
-  // 주차 대수의 수 만큼 정답에 가격을 더한다.
-  if (num === 1) answer += num * a;
-  else if (num === 2) answer += num * b;
-  else if (num === 3) answer += num * c;
+  if (num > 0) {
+    answer += num * prices[num - 1];
+  }
 }
+
 console.log(answer);
+// for (let i = 1; i <= 3; i++) {
+//   times.push(input[i].split(" ").map(Number));
+// }
+
+// // 각 주차 대수마다 가격을 저장한 변수
+// const [a, b, c] = input[0].split(" ").map(Number);
+
+// // 나간시간과 들어온 시간을 각각 배열에 담는다.
+// let inTime = [];
+// let outTime = [];
+// times.forEach((ele) => {
+//   inTime.push(ele[0]);
+//   outTime.push(ele[1]);
+// });
+
+// // 정답 변수
+// let answer = 0;
+// // 현재 주차중인 트럭 대수
+// let num = 0;
+
+// // 주어지는 시간인 1부터 100까지 순회
+// for (let i = 1; i <= 100; i++) {
+//   // 현재 시간을 i라고 놓고 i시간에 들어오는 차와 나가는 차의 대수를 찾음.
+//   const inNum = inTime.filter((ele) => ele === i).length;
+//   const outNum = outTime.filter((ele) => ele === i).length;
+
+//   // 1대라도 있으면 그 대수만큼 현재 주차 대수의 더하거나 뺀다.
+//   if (inNum) {
+//     num += inNum;
+//   }
+//   if (outNum) {
+//     num -= outNum;
+//   }
+
+//   // 주차 대수의 수 만큼 정답에 가격을 더한다.
+//   if (num === 1) answer += num * a;
+//   else if (num === 2) answer += num * b;
+//   else if (num === 3) answer += num * c;
+// }
+// console.log(answer);
