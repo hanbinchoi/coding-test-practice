925685bc97598788e18a8ddb5e122a7f4a298d76
hanbinchoi <wkghskak@naver.com>
2024-03-29 14:53:41 +0900

    문자열 문제풀이

---
+++ 문자열/소문자로바꾸기.js
@@ -0,0 +1,14 @@
+// [문제 이름]
+// : 소문자로 바꾸기
+
+// [문제 설명]
+// : 알파벳으로 이루어진 문자열 myString이 주어집니다. 모든 알파벳을 소문자로 변환하여 return 하는 solution 함수를 완성해 주세요.
+
+// [문제 링크]
+// : https://school.programmers.co.kr/learn/courses/30/lessons/181876
+
+function solution(n) {
+  return n.toLowerCase();
+}
+
+console.log(solution("aBcDeFg"));
